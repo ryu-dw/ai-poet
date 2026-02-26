8f000bc1bbb32df9a9d69eed817db6fb96292ff6
유동원 <dwryu@yudong-won-ui-MacBookPro.local>
2026-02-26 12:55:24 +0900

    환경변수읽는로직수정

--- poet/chapter05/main.py
+++ poet/chapter05/main.py
@@ -6,13 +6,14 @@ sys.path.append(str(PROJECT_ROOT))
 print(str(PROJECT_ROOT))
 
 import os
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from poet.lloa_rest_llm import WiseChatModel
 import streamlit as st
 
-#load_dotenv()
+# load_dotenv()
 LLOA_API_KEY = os.getenv("LLOA_API_KEY")
 
 llm = WiseChatModel(
@@ -33,7 +34,7 @@ chain = prompt | llm | output_parser
 st.title("인공지능 시인")
 content = st.text_input("시의 주제를 입력하세요")
 
-#시 작성 요청하기
+# 시 작성 요청하기
 if st.button("시 작성 요청하기"):
     with st.spinner("Wait for it..."):
         response = chain.invoke({"input": content + "에 대한 시를 작성해줘"})
